407599355e65fd0d85a71ba1d5c985cbcdc8f5e0
prs-dev <pratyushsri.25@gmail.com>
2025-11-02 16:45:39 +0530

    connected server with client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react'
+import {io} from 'socket.io-client'
 
 const App = () => {
+  useEffect(() => {
+    const socket = io('http://localhost:5000')
+    socket.on('connect', () => {
+      console.log("socket connected:", socket.id)
+    })
+    return () => socket.disconnect()
+  }, [])
   return (
     <div>
       <h1>Live Notes</h1>
--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 const express = require("express")
 const {createServer} = require('http')
 const {Server} = require("socket.io")
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 
 const httpServer = createServer(app)
 
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+})
 
 io.on("connection", socket => {
-    
+    console.log('a user connected', socket.id)
+    socket.on('disconnect', () => {
+        console.log("user disconnected")
+    })
 })
 
 httpServer.listen(5000, () => {
